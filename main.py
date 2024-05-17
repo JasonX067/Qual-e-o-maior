9ba2e40ad5e0e6b7851cbcc67a46b57c409d69f8
Jason <166767325+JasonX067@users.noreply.github.com>
2024-05-17 16:50:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+n1 = float(input("digite o 1º número \n "))
+
+n2 = float(input("digite o 2º número \n "))
+
+if n1 == n2:
+
+  print("Para de ser burro e digita dois números diferentes, cabaço")
+
+elif n1 > n2 :
+
+   print(f"{n1} é maior")
+
+else :
+
+   print(f"{n2} é maior")
+
+
+
